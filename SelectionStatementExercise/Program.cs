4479dcdcf24b69a241d7cfe757f6d7df24530416
Robert Rogers <rerogers@gmail.com>
2023-02-21 19:33:13 -0600

    if then exercise 1 updated

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 10); // generates a random: 1 <= favNumber < 10
+            Console.Write("I'm thinking of a random number between 1 and 10. Guess which number: ");
+            var userInput = int.Parse(Console.ReadLine());
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("Too low.");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Too high.");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!!!");
+            }
         }
     }
 }
